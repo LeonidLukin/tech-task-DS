9782a5764dcb98dd07ec133931735689bd863845
Leonid Lukin <leonidlukin90@gmail.com>
2022-11-09 23:58:11 +0200

    instaled material ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,16 @@ import './App.css';
 
 const API_URL = 'https://636bd1aa7f47ef51e13b4541.mockapi.io/api';
 function App() {
-  const [products, setProducts] = useState([]);
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-    fetch(`${API_URL}/products`)
+    fetch(`${API_URL}/users`)
     .then((response) => response.json())
-    .then((json) => setProducts(json));
+    .then((json) => setUsers(json));
   })
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {products.map((product:any) => <div>{product.color}</div>)}
+      {users.map((user:any) => <div>{user.firstName}</div>)}
     </div>
   );
 }
