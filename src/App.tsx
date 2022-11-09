731b57ca08d140b973962b85e8d070059bc96e92
Leonid Lukin <62306922+Ilya-timofeev@users.noreply.github.com>
2022-11-09 18:31:52 +0200

    fetched api products

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const API_URL = 'https://636bd1aa7f47ef51e13b4541.mockapi.io/api';
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(`${API_URL}/products`)
+    .then((response) => response.json())
+    .then((json) => setProducts(json));
+  })
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +26,7 @@ function App() {
           Learn React
         </a>
       </header>
+      {products.map((product:any) => <div>{product.color}</div>)}
     </div>
   );
 }
