9026b4511c729579e033cf71affbd06f4ec2d147
Leonid Lukin <leonidlukin90@gmail.com>
2022-11-10 15:23:32 +0200

    implemented infinity scroll and Loader

---
+++ src/EndMessege/EndMessege.tsx
@@ -0,0 +1,8 @@
+export default function EndMessege() {
+  return (
+        <>
+        <p style={{ textAlign: 'center' }}>
+        <b>That were all users that you have.</b>
+        </p></>
+  );
+}
--- src/UsersTable/UsersTable.tsx
+++ src/UsersTable/UsersTable.tsx
@@ -1,72 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material/';
+import Loader from '../Loader/Loader'
+import EndMessege from '../EndMessege/EndMessege';
 
 const API_URL = 'https://636bd1aa7f47ef51e13b4541.mockapi.io/api';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
 export default function UsersTable() {
+    const [users, setUsers] = useState<any>([]);
+    const [noMore, setnoMore] = useState(true);
+    const [page, setPage] = useState(2);
 
-    const [users, setUsers] = useState([]);
   useEffect(() => {
-    fetch(`${API_URL}/users`)
-    .then((response) => response.json())
-    .then((json) => setUsers(json));
-  })
+    const getUsers = async () => {
+        const res = await fetch(`${API_URL}/users?page=1&limit=20`);
+        const data = await res.json();
+        setUsers(data)
+    }
+    getUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const res = await fetch(`${API_URL}/users?page=${page}&limit=20`);
+    const data = await res.json();
+    return data
+  }
+
+  const fetchData = async () => {
+    const usersFormServe = await fetchUsers();
+
+    setUsers([...users, ...usersFormServe]);
+    if (usersFormServe.length === 0 || usersFormServe.length < 20) {
+        setnoMore(false);
+    }
+    setPage(page + 1)
+  }
   return (
     <TableContainer component={Paper}>
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-            <TableRow>
-                <TableCell align="right">First name</TableCell>
-                <TableCell align="right">Last name</TableCell>
-                <TableCell align="right">Phone number</TableCell>
-                <TableCell align="right">City</TableCell>
-                <TableCell align="right">Email</TableCell>
-            </TableRow>
-        </TableHead>
-        <TableBody>
-      {/* {users.map((user:any) => <div>{user.firstName}</div>)} */}
-      
+        <InfiniteScroll
+            dataLength={users.length} //This is important field to render the next data
+            next={fetchData}
+            hasMore={noMore}
+            loader={<Loader/>}
+            endMessage={<EndMessege/>}
+        >
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell align="right">First name</TableCell>
+                        <TableCell align="right">Last name</TableCell>
+                        <TableCell align="right">Phone number</TableCell>
+                        <TableCell align="right">City</TableCell>
+                        <TableCell align="right">Email</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
 
-        {users.map((users:any) => (
-            <TableRow
-                key={users}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-                <TableCell component="th" scope="row">
-                    {users.firstName}
-                </TableCell>
-                <TableCell align="right">{users.lastName}</TableCell>
-                <TableCell align="right">{users.phoneNumber}</TableCell>
-                <TableCell align="right">{users.city}</TableCell>
-                <TableCell align="right">{users.email}</TableCell>
-            </TableRow>
-        ))}
-        </TableBody>
-    </Table>
+                {users.map(({firstName, id, lastName, phoneNumber, city, email}:any) => (
+                    <TableRow
+                        key={id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                        <TableCell component="th" scope="row">
+                            {firstName}
+                        </TableCell>
+                        <TableCell align="right">{lastName}</TableCell>
+                        <TableCell align="right">{phoneNumber}</TableCell>
+                        <TableCell align="right">{city}</TableCell>
+                        <TableCell align="right">{email}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </InfiniteScroll>
     </TableContainer>
   );
 }
